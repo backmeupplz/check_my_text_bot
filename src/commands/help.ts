f03d058a0289a561b95b6b9d7f5f49e2ee30d343
backmeupplz <backmeupplz@gmail.com>
2019-11-24 08:30:38 -0800

    finish help

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,8 +1,12 @@
 // Dependencies
-import { Telegraf, ContextMessageUpdate } from 'telegraf'
+import Telegraf, { ContextMessageUpdate, Extra } from 'telegraf'
+import { ExtraReplyMessage } from 'telegraf/typings/telegram-types'
 
 export function setupHelp(bot: Telegraf<ContextMessageUpdate>) {
   bot.command(['help', 'start'], ctx => {
-    ctx.replyWithHTML(ctx.i18n.t('help'))
+    ctx.replyWithHTML(
+      ctx.i18n.t('help'),
+      Extra.webPreview(false) as ExtraReplyMessage
+    )
   })
 }
