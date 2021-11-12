a8358f2b4369da60948fcf9724698870ceb879d2
backmeupplz <backmeupplz@gmail.com>
2021-11-11 16:25:28 -0800

    fix middleware

--- src/middlewares/attachUser.ts
+++ src/middlewares/attachUser.ts
@@ -2,6 +2,9 @@ import { findUser } from '../models'
 import { ContextMessageUpdate } from 'telegraf'
 
 export async function attachUser(ctx: ContextMessageUpdate, next: Function) {
+  if (!ctx.from) {
+    return
+  }
   const dbuser = await findUser(ctx.from.id)
   ctx.dbuser = dbuser
   next()
